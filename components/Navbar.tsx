e045596acf33a72d59ca2b85837aa268a22a846f
Eyuale <eyualekifle99@gmail.com>
2024-11-12 00:11:28 -0900

    redesigned navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,23 +1,27 @@
-import { title } from 'process'
+import Link from 'next/link'
 import React from 'react'
+import { LayoutDashboard, ChartColumn, Trophy } from 'lucide-react'
 
 const navitems = [
-    { title: "Dashboard"},
-    { title: "Analytics"},
-    { mi: "Leaderboard"},
-    { mi: "Leaderboard"},
-    { mi: "Leaderboard"},
+    { title: "Dashboard", href: "/dashboard", icon: LayoutDashboard},
+    { title: "Analytics", href: "/analytics", icon: ChartColumn},
+    { title: "Leaderboard", href: "/leaderboard", icon: Trophy }
 ]
 
 
 const Navbar = () => {
   return (
     <div className='bg-slate-900 text-white p-2 flex justify-around items-center h-[100px]'>
-      <h1 className='text-4xl' >Home</h1>
+      <Link href="/" className='text-4xl' >Home</Link>
       <ul className='flex flex-row'>
-        {navitems.map((kal, index) => {
+        {navitems.map((item, index) => {
             return (
-            <li className='mx-10' key={index}>{kal.title}{kal.mi}</li>)
+            <li className='mx-10 hover:bg-slate-700 cursor-pointer p-2 rounded' key={index}>
+              <Link href={item.href} className='flex flex-row gap-2 items-center'>
+              {item.icon && React.createElement(item.icon, { size: 16 })}
+                {item.title}
+              </Link>
+            </li>)
         }
         )}
       </ul>
